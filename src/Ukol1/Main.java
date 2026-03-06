372190ef005dfb9489d620d785c1c8bdc2e4b524
Bim <bimkar95@gasos-ro.cz>
2026-03-06 07:30:36 +0100

    Ukol1

---
+++ src/Ukol1/Main.java
@@ -0,0 +1,17 @@
+package Ukol1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vypiš řetězec");
+        int soucet = 0;
+        for(char a : sc.nextLine().toCharArray()) {
+            System.out.print((int)a + ", ");
+            soucet += (int)a;
+        }
+        System.out.println();
+        System.out.println(soucet);
+    }
+}
